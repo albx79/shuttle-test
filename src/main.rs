aaf5f95063dcf197a9761901cedbbcb93b2cb29b
Alberto Colombo <albx79@gmail.com>
2024-05-12 16:44:51 +0200

    remove all, and make a character sheet

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,118 @@
-use actix_web::{post, Result as AwResult, web};
-use actix_web::{get, web::ServiceConfig};
-use actix_web::error::ErrorInternalServerError;
-use actix_web::web::{Data, Path};
-use maud::{html, Markup};
+use askama::Template;
+use axum::{routing::get, Router};
+use axum::extract::{Path, Query, State};
+use axum::http::StatusCode;
+use axum::response::{Html, IntoResponse, Response};
+use axum::routing::post;
+use tracing::info;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use edgedb_tokio::Client as EdgeClient;
 use serde::Deserialize;
-use shuttle_actix_web::ShuttleActixWeb;
-use shuttle_runtime::CustomError;
-use sqlx::{Executor, FromRow, PgPool};
 
-#[get("/")]
-async fn root(conn: Data<PgPool>) -> AwResult<Markup> {
-    Ok(html! {
-        html {
-            head {
-                link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css";
-                link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0";
-                script src="https://unpkg.com/htmx.org@1.9.5" {}
-            }
-            body {
-                h1 { "Todo List" }
-                div #"notes" {
-                    (get_notes(conn).await?)
-                }
-            }
-        }
-    })
-}
-
-#[get("/notes")]
-async fn notes(conn: Data<PgPool>) -> AwResult<Markup> {
-    get_notes(conn).await
-}
-
-async fn get_notes(conn: Data<PgPool>) -> AwResult<Markup> {
-    let all_notes: Vec<Note> = sqlx::query_as("SELECT id, note FROM todos").fetch_all(conn.get_ref())
-        .await.map_err(|e| ErrorInternalServerError(e))?;
-    Ok(html!{
-        table {
-            tbody {
-                @for note in &all_notes {
-                    tr {
-                        td { (note.note) }
-                        td { a hx-post={"/notes/"(note.id)"/delete"} hx-target="#notes" {
-                            span ."material-symbols-outlined" { "delete" }
-                        } }
-                    }
-                }
-            }
-        }
-        form hx-post="/notes" hx-target="#notes" {
-            input type="text" name="text" autofocus;
-            button type="submit" { "Submit" }
-        }
-    })
-}
-
-#[post("/notes")]
-async fn create_note(conn: Data<PgPool>, web::Form(new_note): web::Form<NewNote>) -> AwResult<Markup> {
-    sqlx::query("INSERT INTO todos(note) VALUES ($1)")
-        .bind(new_note.text)
-        .execute(conn.get_ref())
-        .await.map_err(|e| ErrorInternalServerError(e))?;
-    get_notes(conn).await
-}
-
-#[post("/notes/{id}/delete")]
-async fn delete_note(conn: Data<PgPool>, id: Path<i32>) -> AwResult<Markup> {
-    sqlx::query("DELETE FROM todos WHERE id=$1")
-        .bind(id.into_inner())
-        .execute(conn.get_ref())
-        .await.map_err(|e| ErrorInternalServerError(e))?;
-    get_notes(conn).await
-}
-
-#[derive(Debug, FromRow)]
-struct Note {
-    id: i32,
-    note: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct NewNote {
-    text: String,
+async fn hello_world() -> &'static str {
+    "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn actix_web(
-    #[shuttle_shared_db::Postgres] pool: PgPool,
-) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    pool.execute(include_str!("./schema.sql"))
-        .await
-        .map_err(CustomError::new)?;
+async fn main() -> shuttle_axum::ShuttleAxum {
+    println!("EDGEDB {:?};", std::env::var("EDGEDB_SECRET_KEY"));
+    let client: EdgeClient = edgedb_tokio::create_client().await
+        .map_err(|e| shuttle_runtime::Error::Database(e.to_string()))?;
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "with_axum_htmx_askama=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    let config = move |cfg: &mut ServiceConfig| {
-        cfg
-            .app_data(Data::new(pool))
-            .service(root)
-            .service(notes)
-            .service(create_note)
-            .service(delete_note)
-        ;
-    };
+    info!("initializing router...");
+    let fate_router = Router::new()
+        .route("/characters/:id", get(render_character))
+        .with_state(client.clone());
 
-    Ok(config.into())
+    let router = Router::new()
+        .nest("/fate", fate_router)
+        .with_state(client);
+
+    Ok(router.into())
 }
+
+#[derive(Template)]
+#[template(path = "character-sheet.html")]
+struct CharacterSheet {
+    character: Character,
+    editable: bool,
+}
+
+struct Character {
+    name: String,
+    aspects: Vec<String>,
+    skills: Vec<Skill>,
+    stunts: Vec<String>,
+}
+
+struct Skill {
+    name: String,
+    rating: u8,
+}
+
+#[derive(Deserialize, Default)]
+struct RenderCharacter {
+    #[serde(default)]
+    editable: bool,
+}
+
+async fn render_character(Path(id): Path<String>, Query(params): Query<RenderCharacter>, State(_client): State<EdgeClient>) -> impl IntoResponse {
+    let char = CharacterSheet {
+        character: Character {
+            name: "John Doe".into(),
+            aspects: [
+                "Brave adventurer",
+                "Afraid of the dark",
+                "Clever",
+                "Resourceful",
+            ].iter().map(|s| s.to_string()).collect(),
+            skills: vec![
+                Skill{ name: "Contacts".to_string(), rating: 4 },
+                Skill{ name: "Deceive".to_string(), rating: 3 },
+                Skill{ name: "Provoke".to_string(), rating: 3 },
+                Skill{ name: "Rapport".to_string(), rating: 2 },
+                Skill{ name: "Contacts".to_string(), rating: 2 },
+                Skill{ name: "Shoot".to_string(), rating: 2 },
+            ],
+            stunts: vec![
+                "Acrobatic Maneuver: Once per session, gain +2 to Athletics for a daring physical feat.".to_string(),
+                "Master of Disguise: Gain +2 to Deceive when attempting to disguise yourself.".to_string(),
+                "Keen Observer: Once per scene, reroll any failed Notice check.".to_string(),
+            ],
+        },
+        editable: params.editable,
+    };
+    HtmlTemplate(char)
+}
+
+/// Allows us to convert Askama HTML templates into valid HTML for axum to serve in the response.
+impl<T> IntoResponse for HtmlTemplate<T>
+    where
+        T: Template,
+{
+    fn into_response(self) -> Response {
+        // Attempt to render the template with askama
+        match self.0.render() {
+            // If we're able to successfully parse and aggregate the template, serve it
+            Ok(html) => Html(html).into_response(),
+            // If we're not, return an error or some bit of fallback HTML
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
+}
+
+/// A wrapper type that we'll use to encapsulate HTML parsed by askama into valid HTML for axum to serve.
+struct HtmlTemplate<T>(T);
+
